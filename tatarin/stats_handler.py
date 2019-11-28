00233e20207ec964e444b6eada930b72295b268d
ufian <ufian@ya.ru>
2019-11-27 22:25:10 -0800

    Add local stat

--- tatarin/stats_handler.py
+++ tatarin/stats_handler.py
@@ -10,9 +10,6 @@ def _is_stats_request(data):
     msg = data['text']
     msg_lower = msg.lower().strip()
 
-    if not is_direct_message(data):
-        return False
-
     return 'статистика' == msg_lower
 
 def get_stats():
@@ -33,9 +30,33 @@ def get_stats():
 
     return "\n".join(lines)
 
+def get_local_stats(data):
+    user = data['user']
+    channel = data['channel']
+    stats = Messages.objects(data__exists=True, user=user).aggregate(
+        {"$match": {"data.channel": channel}},
+        {"$group": {
+            "_id": "$user",
+            "count": {"$sum": 1}
+        }},
+        {"$sort": {
+            "count": -1
+        }}
+    )
+
+    lines = [
+        "<@{0}> отправил(а) {1} сообщений в этот чат".format(row["_id"], row["count"])
+        for row in stats
+    ]
+
+    return "\n".join(lines)
+
 
 def handler(data):
     if not _is_stats_request(data):
         return None
 
-    return get_stats()
+    if is_direct_message(data):
+        return get_stats()
+
+    return get_local_stats(data)
